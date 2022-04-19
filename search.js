3a609dc446eb15190edcc5bbb2720e5c9009c54a
Lauren Knight <lknight2@mail.umw.edu>
2022-04-18 20:53:10 -0400

    Search fixed

--- search.js
+++ search.js
@@ -1,24 +1,25 @@
 function search_update() {
-    var text = document.getElementById('searchbox').value;
+    var text = document.getElementById('searchbox').value.trim();
     console.log(text);
     if (text != null && text.length > 0) {
         search(text);
     } else {
         var container = document.getElementById('button-container');
         for (const element of container.children) {
-            element.children[0].style.visibility = "visible";
+            element.style.display = "block";
         }
     }
 }
 
 function search(keywordString) {
-    var words = keywordString.split(' ');
+    var words = keywordString.split(/\s+/);
     var container = document.getElementById('button-container');
     for (const element of container.children) {
-        var keywords = element.children[0].getAttribute('keywords').split(' ');
+        var keywords = element.children[0].getAttribute('keywords').split(/\s+/);
         var found = false;
         for (const keyword of keywords) {
-            for (const word of words) {
+            for (var word of words) {
+                word = word.toLowerCase();
                 if (keyword.includes(word)) {
                     found = true;
                     break;
@@ -28,8 +29,10 @@ function search(keywordString) {
                 break;
             }
         }
-        if (!found) {
-            element.style.visibility = "hidden";
+        if (found) {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
         }
     }
 }
